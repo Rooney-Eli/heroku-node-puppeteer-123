eb164a229dbc2f9971ff51db2d17a6349eb91a85
Rooney-Eli <rooney.developer@gmail.com>
2022-08-23 20:08:51 -0600

    made real

--- bot.js
+++ bot.js
@@ -7,10 +7,21 @@ const bot = async () => {
         args: ['--no-sandbox']
     })
     const page = await browser.newPage()
-    await page.setContent(`<p>web running at ${Date()}</p>`)
-    const content = await page.content()
+    await page.goto("https://playoverwatch.com/en-us/career/pc/Zeno-11154/")
+    await page.waitForNetworkIdle({idleTime: 1})
+
+
+    const x = await page.evaluate(() => {
+        const roleRanks = Array.from(document.querySelectorAll('#overview-section > div > div.u-max-width-container.row.content-box.gutter-18 > div > div > div.masthead-player > div > div.competitive-rank > div.competitive-rank-role'))
+
+        //order of the role ranks will not be consistent. use the 1nth child to determine which role is currently being parsed
+        return roleRanks.map((roleRank) => {
+            return roleRank.querySelector('div:nth-child(2) > div.competitive-rank-level').innerText
+        })
+    })
+
     await browser.close()
-    return content
+    return x
 
 }
 
